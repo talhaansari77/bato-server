b12afd100c15ec51e3075d057783c145c7fd1a0f
Ultron <talhaprince09@gmail.com>
2026-06-08 23:14:44 +0300

    Add ASP.NET Identity JWT auth and BATO seed data

--- obj/Debug/net9.0/BatoClinic.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/BatoClinic.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatoClinic.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13413d234498a8180fb15d2444ec4ddcbc866505")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatoClinic.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatoClinic.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
